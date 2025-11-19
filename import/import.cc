f0776438b840dea27e0ebf11cc8f3b359ee374db
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2025-11-19 15:36:32 +0000

    Start adding options for reference experiment

--- import/import.cc
+++ import/import.cc
@@ -16,69 +16,102 @@
 #include <format>
 #include <sstream>
 
+
+#pragma region Argument Parser
+class ImportArgumentParser : public argparse::ArgumentParser {
+    public:
+        ImportArgumentParser(std::string version) : ArgumentParser("", version, argparse::default_arguments::help) {
+            add_importer_arguments();
+        }
+    private:
+        void add_importer_arguments(){
+            add_argument("file").help("Path to the nexus file").metavar("FILE.nxs");
+            // Reference geometry options
+            add_argument("--reference-geometry")
+                .help("Path to a reference experiment geometry");
+            add_argument("--reference.use-beam")
+                .help("Control whether the beam model from the reference is used")
+                .nargs(0, 1)
+                .default_value(true)
+                .implicit_value(true);
+            add_argument("--reference.use-goniometer")
+                .help("Control whether the goniometer model from the reference is used")
+                .nargs(0, 1)
+                .default_value(true)
+                .implicit_value(true);
+            add_argument("--reference.use-detector")
+                .help("Control whether the detector model from the reference is used")
+                .nargs(0, 1)
+                .default_value(true)
+                .implicit_value(true);
+            // Beam options
+            add_argument("-w", "--wavelength", "--beam.wavelength")
+                .help("Wavelength of the X-ray beam (Å)")
+                .scan<'f', float>();
+            add_argument("--beam.direction")
+                .help("Sample to source direction of the X-ray beam")
+                .nargs(3)
+                .scan<'f', float>();
+            add_argument("--beam.divergence")
+                .help("Divergence of the X-ray beam") // FIXME - units?
+                .scan<'f', float>();
+            add_argument("--beam.sigma-divergence")
+                .help("Sigma divergence of the X-ray beam") // FIXME - what exactly is this, what are units?
+                .scan<'f', float>();
+            add_argument("--beam.polarization-normal")
+                .help("Polarization vector of the X-ray beam")
+                .nargs(3)
+                .scan<'f', float>();
+            add_argument("--beam.polarization-fraction")
+                .help("Polarization fraction of the X-ray beam")
+                .scan<'f', float>();
+            add_argument("--beam.flux")
+                .help("Incident flux of the X-ray beam") // FIXME - units?
+                .scan<'f', float>();
+            add_argument("--beam.transmission")
+                .help("Transmission of the X-ray beam") // FIXME - units?
+                .scan<'f', float>();
+            // Scan options
+            add_argument("--image-range", "--scan.image-range")
+                .help("The subset of images from the nxs file for processing")
+                .nargs(2)
+                .scan<'i', int>();
+            add_argument("--scan.oscillation-start")
+                .help("The starting angle of the scan (°)")
+                .scan<'f', float>();
+            add_argument("--scan.oscillation-width")
+                .help("The rotation width of each image in the scan (°)")
+                .scan<'f', float>();
+            // Goniometer options
+            add_argument("--axis", "--goniometer.single.axis") // primary indicator of single axis gonio.
+                .nargs(3)
+                .scan<'g', double>();
+            add_argument("--fixed-rotation", "--goniometer.single.fixed-rotation") // defaults to identitiy
+                .nargs(9)
+                .scan<'g', double>();
+            add_argument("--setting-rotation", "--goniometer.single.setting-rotation") // defaults to identitiy
+                .nargs(9)
+                .scan<'g', double>();
+            add_argument("--axes", "--goniometer.multi.axes") // primary indicator of multi axis gonio (if single axis given, then equivalent to single axis gonio).
+                .nargs(argparse::nargs_pattern::at_least_one)
+                .scan<'g', double>();
+            add_argument("--angles", "--goniometer.multi.angles") // defaults to 0 for each axis
+                .nargs(argparse::nargs_pattern::at_least_one)
+                .scan<'g', double>();
+            add_argument("--names", "--goniometer.multi.names") // defaults to "" for each axis
+                .nargs(argparse::nargs_pattern::at_least_one);
+            add_argument("--scan-axis", "--goniometer.multi.scan-axis") // defaults to 0
+                .scan<'u', uint32_t>();
+        }
+};
+
+#pragma endregion
+
+
 int main(int argc, char **argv) {
     // This program creates dx2 experiment models from nxmx-format data.
     auto t1 = std::chrono::system_clock::now();
-    auto parser = argparse::ArgumentParser();
-    parser.add_argument("file").help("Path to the nexus file").metavar("FILE.nxs");
-    // Beam options
-    parser.add_argument("-w", "--wavelength", "--beam.wavelength")
-        .help("Wavelength of the X-ray beam (Å)")
-        .scan<'f', float>();
-    parser.add_argument("--beam.direction")
-        .help("Sample to source direction of the X-ray beam")
-        .nargs(3)
-        .scan<'f', float>();
-    parser.add_argument("--beam.divergence")
-        .help("Divergence of the X-ray beam") // FIXME - units?
-        .scan<'f', float>();
-    parser.add_argument("--beam.sigma-divergence")
-        .help("Sigma divergence of the X-ray beam") // FIXME - what exactly is this, what are units?
-        .scan<'f', float>();
-    parser.add_argument("--beam.polarization-normal")
-        .help("Polarization vector of the X-ray beam")
-        .nargs(3)
-        .scan<'f', float>();
-    parser.add_argument("--beam.polarization-fraction")
-        .help("Polarization fraction of the X-ray beam")
-        .scan<'f', float>();
-    parser.add_argument("--beam.flux")
-        .help("Incident flux of the X-ray beam") // FIXME - units?
-        .scan<'f', float>();
-    parser.add_argument("--beam.transmission")
-        .help("Transmission of the X-ray beam") // FIXME - units?
-        .scan<'f', float>();
-    // Scan options
-    parser.add_argument("--image-range", "--scan.image-range")
-        .help("The subset of images from the nxs file for processing")
-        .nargs(2)
-        .scan<'i', int>();
-    parser.add_argument("--scan.oscillation-start")
-        .help("The starting angle of the scan (°)")
-        .scan<'f', float>();
-    parser.add_argument("--scan.oscillation-width")
-        .help("The rotation width of each image in the scan (°)")
-        .scan<'f', float>();
-    // Goniometer options
-    parser.add_argument("--axis", "--goniometer.single.axis") // primary indicator of single axis gonio.
-        .nargs(3)
-        .scan<'g', double>();
-    parser.add_argument("--fixed-rotation", "--goniometer.single.fixed-rotation") // defaults to identitiy
-        .nargs(9)
-        .scan<'g', double>();
-    parser.add_argument("--setting-rotation", "--goniometer.single.setting-rotation") // defaults to identitiy
-        .nargs(9)
-        .scan<'g', double>();
-    parser.add_argument("--axes", "--goniometer.multi.axes") // primary indicator of multi axis gonio (if single axis given, then equivalent to single axis gonio).
-        .nargs(argparse::nargs_pattern::at_least_one)
-        .scan<'g', double>();
-    parser.add_argument("--angles", "--goniometer.multi.angles") // defaults to 0 for each axis
-        .nargs(argparse::nargs_pattern::at_least_one)
-        .scan<'g', double>();
-    parser.add_argument("--names", "--goniometer.multi.names") // defaults to "" for each axis
-        .nargs(argparse::nargs_pattern::at_least_one);
-    parser.add_argument("--scan-axis", "--goniometer.multi.scan-axis") // defaults to 0
-        .scan<'u', uint32_t>();
+    auto parser = ImportArgumentParser("1.0");
 
     try {
         parser.parse_args(argc, argv);
@@ -100,6 +133,36 @@ int main(int argc, char **argv) {
     // Bind this as a reference
     Reader &reader = *reader_ptr;
 
+#pragma region Reference experiment
+    // Parse the reference experiment if provided.
+    Experiment<MonochromaticBeam> reference_expt;
+    bool use_reference_beam = false;
+    bool use_reference_goniometer = false;
+    bool use_reference_detector = false;
+    if (parser.is_used("reference-geometry")){
+        std::string reference_file = parser.get<std::string>("reference-geometry");
+        std::ifstream f(reference_file);
+        json reference_json;
+        try {
+            reference_json = json::parse(f);
+        } catch (json::parse_error &ex) {
+            logger.error("Unable to read {}; json parse error at byte {}",
+                        reference_file.c_str(),
+                        ex.byte);
+            std::exit(1);
+        }
+        try {
+            reference_expt = Experiment<MonochromaticBeam>(reference_json);
+        } catch (std::invalid_argument const &ex) {
+            logger.error("Unable to create MonochromaticBeam experiment: {}", ex.what());
+            std::exit(1);
+        }
+        use_reference_beam = parser.get<bool>("reference.use-beam");
+        use_reference_goniometer = parser.get<bool>("reference.use-goniometer");
+        use_reference_detector = parser.get<bool>("reference.use-detector");
+    }
+#pragma endregion
+
     // Initialise the experiment and generate an identifier.
     Experiment<MonochromaticBeam> expt;
     expt.generate_identifier();
@@ -110,18 +173,23 @@ int main(int argc, char **argv) {
     // is available from the reader.
 
 #pragma region Beam
-    
+    float wavelength = 0.0;
     json beam_data;
-    float wavelength;
-    
+    // If reference, use this to create a beam data json, which can be later updated with custom options.
+    if (use_reference_beam){
+        beam_data = reference_expt.beam().to_json();
+        wavelength = reference_expt.beam().get_wavelength();
+        logger.info("Using reference beam model as starting model.");
+    }
+
     // wavelength MUST be provided/found, as there is no sensible default.
     if (parser.is_used("beam.wavelength")){
         wavelength = parser.get<float>("beam.wavelength");
     }
-    else if (reader.get_wavelength().has_value()){
+    if (wavelength <= 0.0 && reader.get_wavelength().has_value()){
         wavelength = reader.get_wavelength().value();
     }
-    else {
+    if (wavelength <= 0.0){
         throw std::runtime_error("No wavelength value found in file, and not provided as an input option with --beam.wavelength");
     }
     beam_data["wavelength"] = wavelength;
@@ -131,6 +199,7 @@ int main(int argc, char **argv) {
     if (parser.is_used("beam.direction")){
         beam_data["direction"] = parser.get<std::vector<float>>("beam.direction");
     }
+    // Then if direction not in beam_data - know not reference or user option, then can see if anything from reader.
     if (parser.is_used("beam.divergence")){
         beam_data["divergence"] = parser.get<float>("beam.divergence");
     }
@@ -208,69 +277,93 @@ int main(int argc, char **argv) {
 #pragma region Goniometer
 
     Goniometer goniometer;
+    json goniometer_data;
+    // If reference, use this to create a goniometer data json, which can be later updated with custom options.
+    // Need to be slightly careful about single/multi axis goniometers - we only want one type of data to be
+    // in the goniometer json.
+    if (use_reference_goniometer){
+        goniometer_data = reference_expt.goniometer().to_json();
+        logger.info("Using reference goniometer model as starting model.");
+    }
 
     if (parser.is_used("goniometer.single.axis")){
-        auto vec = parser.get<std::vector<double>>("goniometer.single.axis");
-        Vector3d axis = {vec[0], vec[1], vec[2]};
-        Matrix3d setting_rotation;
-        Matrix3d fixed_rotation;
-        setting_rotation << 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0;
-        fixed_rotation << 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0;
+        if (goniometer_data.contains("axes")){
+            throw std::invalid_argument("A multi-axis reference goniometer has been provided alongside single-axis options.");
+        }
+        std::vector<double> axis = parser.get<std::vector<double>>("goniometer.single.axis");
+        goniometer_data["rotation_axis"] = axis;
+        std::vector<double> setting_rotation = {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0};
+        std::vector<double> fixed_rotation = {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0};
         if (parser.is_used("goniometer.single.setting-rotation")){
-            auto s_r = parser.get<std::vector<double>>("goniometer.single.setting-rotation");
-            setting_rotation << s_r[0], s_r[1], s_r[2], s_r[3], s_r[4], s_r[5], s_r[6], s_r[7], s_r[8];
+            setting_rotation = parser.get<std::vector<double>>("goniometer.single.setting-rotation");
         }
+        // FIXME else get from reader?
+        goniometer_data["setting_rotation"] = setting_rotation;
         if (parser.is_used("goniometer.single.fixed-rotation")){
-            auto f_r = parser.get<std::vector<double>>("goniometer.single.fixed-rotation");
-            fixed_rotation << f_r[0], f_r[1], f_r[2], f_r[3], f_r[4], f_r[5], f_r[6], f_r[7], f_r[8];
+            fixed_rotation = parser.get<std::vector<double>>("goniometer.single.fixed-rotation");
         }
-        goniometer = Goniometer(fixed_rotation, axis, setting_rotation);
+        // FIXME else get from reader?
+        goniometer_data["fixed_rotation"] = fixed_rotation;
+        
+        goniometer = Goniometer(goniometer_data);
         expt.set_goniometer(goniometer);
         logger.info("Created a single-axis goniometer model with rotation axis ({:.3f}, {:.3f}, {:.3f})", axis[0], axis[1], axis[2]);
     }
     else if (parser.is_used("goniometer.multi.axes")){
-        std::vector<Vector3d> axes;
-        std::vector<double> angles;
-        std::vector<std::string> names;
-        std::size_t scan_axis = 0;
+        if (goniometer_data.contains("axis")){
+            throw std::invalid_argument("A single-axis reference goniometer has been provided alongside multi-axis options.");
+        }
         std::vector<double> input_axes = parser.get<std::vector<double>>("goniometer.multi.axes");
         if (input_axes.size() % 3 != 0){
             throw std::invalid_argument("The number of values input to parameter goniometer.multi.axes must be a multiple of three, as it is a list of vectors");
         }
-        for (int i=0;i<input_axes.size() / 3;++i){
-            axes.push_back(Vector3d(input_axes[i*3], input_axes[i*3+1], input_axes[i*3+2]));
+        std::vector<Vector3d> axes; 
+        int n_axes = input_axes.size() / 3;
+        for (int i=0;i<n_axes; ++i){
+            axes.push_back(Vector3d{input_axes[i*3],input_axes[i*3+1],input_axes[i*3+2]});
         }
+        goniometer_data["axes"] = axes;
+    
         if (parser.is_used("goniometer.multi.angles")){
             std::vector<double> input_angles = parser.get<std::vector<double>>("goniometer.multi.angles");
-            if (input_angles.size() != axes.size()){
-                throw std::invalid_argument(std::format("The number of angles provided ({}) must match the number of axes ({})", input_angles.size(), axes.size()));
+            if (input_angles.size() != n_axes){
+                throw std::invalid_argument(std::format("The number of angles provided ({}) must match the number of axes ({})", input_angles.size(), n_axes));
             }
-            angles = input_angles;
+            goniometer_data["angles"] = input_angles;
         }
-        else {
-            for (int i=0;i<axes.size();++i){
+        else if (!(goniometer_data.contains("angles"))){
+            std::vector<double> angles;
+            for (int i=0;i<n_axes;++i){
                 angles.push_back(0.0);
             }
+            goniometer_data["angles"] = angles;
         }
         if (parser.is_used("goniometer.multi.names")){
             std::vector<std::string> input_names = parser.get<std::vector<std::string>>("goniometer.multi.names");
-            if (input_names.size() != axes.size()){
-                throw std::invalid_argument(std::format("The number of names provided ({}) must match the number of axes ({})", input_names.size(), axes.size()));
+            if (input_names.size() != n_axes){
+                throw std::invalid_argument(std::format("The number of names provided ({}) must match the number of axes ({})", input_names.size(), n_axes));
             }
-            names = input_names;
+            goniometer_data["names"] = input_names;
         }
-        else {
-            for (int i=0;i<axes.size();++i){
+        else if (!(goniometer_data.contains("names"))){
+            std::vector<std::string> names;
+            for (int i=0;i<n_axes;++i){
                 names.push_back("");
             }
+            goniometer_data["names"] = names;
         }
         if (parser.is_used("goniometer.multi.scan-axis")){
-            scan_axis = static_cast<std::size_t>(parser.get<uint32_t>("goniometer.multi.scan-axis"));
-            if (scan_axis >= axes.size()){
-                throw std::invalid_argument(std::format("The specified scan axis index ({}) must be lower than the number of axes ({}).", scan_axis, axes.size()));
+            std::size_t scan_axis = static_cast<std::size_t>(parser.get<uint32_t>("goniometer.multi.scan-axis"));
+            if (scan_axis >= n_axes){
+                throw std::invalid_argument(std::format("The specified scan axis index ({}) must be lower than the number of axes ({}).", scan_axis, n_axes));
             }
+            goniometer_data["scan_axis"] = scan_axis;
         }
-        goniometer = Goniometer(axes, angles, names, scan_axis);
+        else if (!(goniometer_data.contains("scan_axis"))){
+            std::size_t scan_axis = 0;
+            goniometer_data["scan_axis"] = scan_axis;
+        }
+        goniometer = Goniometer(goniometer_data);
         expt.set_goniometer(goniometer);
         logger.info("Created a multi-axis goniometer model");
     }
@@ -304,9 +397,6 @@ int main(int argc, char **argv) {
     std::string material = "Si";
     double mu = calculate_mu_for_material_at_wavelength(material, wavelength);
 
-    
-    
-    // FIXME get rotation axes and update gonio
     std::array<int, 2> image_size = {width, height};
     Panel panel(distance, beam_center_array, 
         pixel_size_array, image_size,
@@ -314,7 +404,6 @@ int main(int argc, char **argv) {
     std::vector<Panel> panels = {panel};
     Detector detector(panels);
     expt.set_detector(detector);
-    
 
     json elist_out = expt.to_json();
     std::string efile_name = "imported.expt";
