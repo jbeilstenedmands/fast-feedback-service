3b255e4baf69803465e061d4bea19b8329e533fe
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2026-09-04 10:26:24 +0100

    add python script

---
+++ src/ffs/ssx_integrate.py
@@ -0,0 +1,233 @@
+import argparse
+import json
+import sys
+import time
+from pathlib import Path
+
+import h5py
+import numpy as np
+
+
+import ffs.index
+import ffs.integrate
+
+def run(args=None):
+    st = time.time()
+    parser = argparse.ArgumentParser(
+        prog="index",
+        description="Runs standalone indexing of serial data using the GPU fast-feedback-indexer",
+        epilog="Text at the bottom of help",
+    )
+    parser.add_argument("-r", "--reflections", help="Path to the strong spots h5 file")
+    parser.add_argument("-e", "--experiments", help="Path to the imported.expt json")
+    parser.add_argument("--test", action="store_true", help="Run in test mode")
+
+    parsed = parser.parse_args(args)
+    #min_spots = parsed.min_spots
+    if not parsed.experiments:
+        print("No imported experiment list provided.")
+        return
+    with open(parsed.experiments, "r") as f:
+        expts = json.load(f)
+    wavelength = expts["beam"][0]["wavelength"]
+    crystals = expts["crystal"]
+    s0 = np.asarray([0.0, 0.0, -1.0 / wavelength], dtype=np.float64)
+    detector_dict = expts["detector"][0]["hierarchy"]
+    # only single panel detectors for now.
+    panel_dict = expts["detector"][0]["panels"][0]
+    detector = {
+        "distance": -1.0 * (detector_dict["origin"][2] + panel_dict["origin"][2]),
+        "beam_center_x": -1.0
+        * (detector_dict["origin"][0] + panel_dict["origin"][0])
+        / panel_dict["pixel_size"][0],
+        "beam_center_y": (detector_dict["origin"][1] + panel_dict["origin"][1])
+        / panel_dict["pixel_size"][1],
+        "pixel_size_x": panel_dict["pixel_size"][0],
+        "pixel_size_y": panel_dict["pixel_size"][1],
+        "image_size_x": panel_dict["image_size"][0],
+        "image_size_y": panel_dict["image_size"][1],
+        "thickness": panel_dict["thickness"],
+        "mu": panel_dict["mu"],
+    }
+
+    '''cell = gemmi.UnitCell(*parsed.cell)
+    input_cell = np.reshape(
+        np.array(cell.orth.mat, dtype="float32"), (3, 3)
+    )  ## As an orthogonalisation matrix.'''
+
+    if not parsed.reflections:
+        print("No strong reflections h5 file provided.")
+        return
+    try:
+        with h5py.File(parsed.reflections, "r") as refls:
+            processing_group = refls["dials"]["processing"]["group_0"]
+            xyz_obs = processing_group["xyzobs.px.value"][:]
+            xyz_cal = processing_group["xyzcal.px"][:]
+            spot_covars = processing_group["spot_covariance"][:]
+            spot_mobs = processing_group["spot_mobs"][:]
+            intensities = processing_group["intensity.sum.value"][:]
+            miller_index = processing_group["miller_index"][:]
+            ids = processing_group["id"][:]
+            experiment_ids = processing_group.attrs["experiment_ids"]
+            identifiers = processing_group.attrs["identifiers"]
+            identifiers_map = dict(zip(experiment_ids, identifiers))
+    except Exception as e:
+        print(
+            f"Unable to interpret the reflection file - please check input.\n Error: {e}"
+        )
+        return
+
+    try:
+        panel = ffs.index.make_panel(
+            detector["distance"],
+            detector["beam_center_x"],
+            detector["beam_center_y"],
+            detector["pixel_size_x"],
+            detector["pixel_size_y"],
+            detector["image_size_x"],
+            detector["image_size_y"],
+            detector["thickness"],
+            detector["mu"],
+        )
+    except Exception as e:
+        print(
+            f"Unable to compose a detector panel model from the detector json.\n Error: {e}"
+        )
+        return
+
+    #output_aggregator = OutputAggregator(identifiers_map)
+    tables = []
+    id_values = []
+
+    ## Note this assumes ids are in ascending order, which is the
+    ## expected form of the output from spotfinding.
+    unique_ids, start_indices = np.unique(ids, return_index=True)
+    end_indices = np.append(start_indices[1:], len(ids))
+    for id_, start, end in zip(unique_ids, start_indices, end_indices):
+        xyz_obs_this = xyz_obs[start:end]
+        xyz_cal_this = xyz_cal[start:end]
+        covars_this = spot_covars[start:end]
+        mobs_this = spot_mobs[start:end]
+        intensities_this = intensities[start:end]
+        miller_index_this = miller_index[start:end]
+        if xyz_obs_this.any():
+            tables.append((xyz_obs_this, xyz_cal_this, covars_this, intensities_this, miller_index_this, mobs_this))
+            id_values.append(id_)
+
+    ## Initialise the GPU integrator.
+    '''try:
+        indexer = GPUIndexer()
+    except ModuleNotFoundError as e:  # if ffbidx not sourced
+        print(f"ModuleNotFoundError: {e}")
+        print(
+            "ffbidx not found, has the fast-feedback-indexer module been built and sourced?"
+        )
+        return
+    except ImportError as e:
+        print(f"ImportError: {e}")
+        print(
+            "Potential source of this error: has the CUDA Runtime Library been loaded?"
+        )
+        return'''
+    '''indexer.panel = panel
+    indexer.cell = input_cell
+    indexer.wavelength = wavelength
+
+    # Quantities to log for log output
+    n_indexed_images = 0
+    n_total = len(tables)'''
+    n_considered = 0
+
+    t1 = time.time()
+
+    for t, i, xtal in zip(tables, id_values, crystals):
+        # data already has an initial prediction.
+        xyzobs_this, xyzcal_this, covars_this, intensities_this, miller_index_this, mobs_this = t
+        obs_flat = xyzobs_this.flatten()
+        cal_flat = xyzcal_this.flatten()
+        covars_flat = covars_this.flatten()
+        #if t.shape[0] < min_spots:
+        #    continue
+        n_considered += 1
+        
+        #print(obs_flat.size)
+        #print(covars_flat)
+        covars = covars_flat.reshape(-1,3)
+        sigma_b = np.mean((covars[0,:] + covars[1,:]) / 2)**0.5
+        # now need rmsd deviation
+        st = time.time()
+        a = xtal["real_space_a"]
+        b = xtal["real_space_b"]
+        c = xtal["real_space_c"]
+        A_inv = np.array([a,b,c], dtype="float64")
+        A = np.linalg.inv(A_inv)
+        ffs.integrate.ssx_integrate(xyzcal_this,
+            xyzobs_this, covars_this, intensities_this, miller_index_this, mobs_this, s0, panel, A
+        )
+        end = time.time()
+        print(f"{end-st:8f}s")
+        '''sigmb_rmsd = ffs.integrate.estimate_rmsd_sigma(
+            xyzcal_this, xyzobs_this,s0,
+            panel)
+        print(sigmb_rmsd)
+        print(((sigmb_rmsd**2) + (sigma_b**2))**0.5)'''
+
+        '''sel = ffs.integrate.max_separation_filter(obs_flat, cal_flat, 2)
+        sel_ob = obs_flat[sel]
+        print(f"{obs_flat.size} {sel_ob.size}")
+        assert 0'''
+
+        # now make "refiner data"
+        # need s0, miller index, intensity, xy_px_obs, xy_pz_cal, covariance_obs, 
+
+        #sp, ctot, xbar, Sobs = reflection_statistics(
+        #        panel, xyz, s0_length, experiment.beam.get_s0(), sbox[r]
+        #    )
+
+        '''result = indexer.index(data)
+        if result.lattices:
+            n_indexed_images += 1
+            lattice = result.lattices[0]
+            # now save stuff for output
+            output_aggregator.add_result(lattice, i)
+            # print number of spots indexed and rmsds
+            rmsdx, rmsdy, rmsd_psi = lattice.rmsds
+            cell_str = ", ".join(f"{i:.3f}" for i in lattice.unit_cell)
+            print(
+                f"Indexed {(lattice.n_indexed)}/{int(data.size / 3)} spots on image {i + 1}:\n"
+                + f"  cell: {cell_str}\n"
+                + f"  RMSDs: (x(px), y(px), psi(rad)): {rmsdx:.3f}, {rmsdy:.3f}, {rmsd_psi:.5f}"
+            )
+        else:
+            print(f"No indexing solution for image {i + 1}")'''
+
+    t2 = time.time()
+    print(f"Mosaicity models refined on {n_considered} images")
+
+    # ideally would generate an indexed.expt type file...
+    # ok say we have in imported.expt, need to add crystals to indexed images
+    '''if parsed.test:
+        with open("indexed_crystals.json", "w") as f:
+            json.dump(output_aggregator.output_crystals_list, f, indent=2)
+    else:
+        expts["crystal"] = output_aggregator.output_crystals_list
+        for i, id_ in enumerate(output_aggregator.output_crystals_id_nos):
+            expts["experiment"][id_]["crystal"] = i
+        with open("indexed.expt", "w") as f:
+            json.dump(expts, f, indent=2)
+
+    # output in standard dials format so that can be understood by dials.
+    if not output_aggregator.ids_output:
+        print("No images successfully indexed, no reflection output will be written.")
+    else:
+        output_aggregator.write_table("indexed.refl")
+    t3 = time.time()
+    print(
+        f"Setup time: {t1 - st:.3f}s, index time {t2 - t1:.3f}s, write time {t3 - t2:.3f}s"
+    )'''
+
+
+if __name__ == "__main__":
+    st = time.time()
+    run(sys.argv[1:])
+    print(f"Program time: {time.time() - st:.3f}s")
